49b056140200540f9a2a82fde6e896b85e9d3e52
sohelraja <rohan.nsec@gmail.com>
2019-07-04 23:57:04 +0530

    SHOW routes added and show.ejs file created

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ app.set("view engine","ejs");
 //Set up schema
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 var Campground  = mongoose.model("Campground",campgroundSchema);//mongoose.model("db_name",mongoose.schema({...}));
 /*Campground.create({
     name: "Rohan",
-    image:"https://pixabay.com/get/55e4d5454b51ab14f6da8c7dda793f7f1636dfe2564c704c732d7ad29644c25b_340.jpg"
+    image:"https://pixabay.com/get/55e4d5454b51ab14f6da8c7dda793f7f1636dfe2564c704c732d7ad29644c25b_340.jpg",
+    description:"This is awesome. This is the best campground."
     },function(err,campground){
         if(err){
             console.log(err);
@@ -25,27 +27,36 @@ var Campground  = mongoose.model("Campground",campgroundSchema);//mongoose.model
             console.log(campground);
         }
     });*/
-//Routes
+///////Home Routes
 app.get('/',function(req,res){
     res.render("landing.ejs");
 });
+///////INDEX ROUTES-> Show all campgrounds from DB
 app.get("/campgrounds",function(req,res){
     //get all Campgrounds fron DB
     Campground.find({},function(err,allCampgrounds){
         if(err){
             console.log(err);
         }else{
-            res.render('campgrounds.ejs',{campgrounds: allCampgrounds});
+            //It will render to index.ejs file 
+            //with allCampgrounds which is stored inside campgrounds array,
+            //I Will access that array inside the index.ejs file
+            res.render('index.ejs',{campgrounds: allCampgrounds});
         }
     });
 });
+//////NEW ROUTES-> Show form to create new campground
+app.get("/campgrounds/new",function(req,res){
+    res.render("new.ejs");
+});
+//////CREATE ROUTES-> Add new campground to DB
 app.post("/campgrounds",function(req,res){
     //This post route accepts the data from new.ejs and redirect to (campgrounds get route)
-
     //get data from form and add to campground array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name:name,image:image};
+    var des = req.body.description;
+    var newCampground = {name:name,image:image,description:des};
     //Create a new campground and save to DataBase
     Campground.create(newCampground,function(err,newlyCreated){
         if(err){
@@ -56,8 +67,18 @@ app.post("/campgrounds",function(req,res){
         }
     });
 });
-app.get("/campgrounds/new",function(req,res){
-    res.render("new.ejs");
+///////SHOW ROUTES-> Show info about one specific campground
+app.get("/campgrounds/:id",function(req,res){
+    //Find the campground with provided id 
+    Campground.findById(req.params.id,function(err,foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            //Render show template with that campground
+            //foundCampground will store inside the campground object, which we will access inside show.ejs file
+            res.render("show.ejs",{campground: foundCampground});
+        }
+    });
 });
 app.listen(3000,function(){
     console.log("YelpCamp server has started.");
